8f330b72b84ab6301c65b7eed749317affd83278
Bokobo Shahni <shahni@bokobo.space>
2021-02-23 16:45:29 -0500

    Fix for Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Devise.setup do |config|
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
   require 'devise/orm/active_record'
 
   config.timeout_in = 30.minutes
